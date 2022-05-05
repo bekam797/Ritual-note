90bae84e483d22422b9a1d4d9b9247913daec7bf
Beka <bekamakharoblishvili@gmail.com>
2022-05-05 16:36:01 +0400

    Fix delete backward function

--- src/plugins/withKeyCommands.js
+++ src/plugins/withKeyCommands.js
@@ -5,17 +5,20 @@ import { createParagraphNode } from "../utils/paragraph";
 const withKeyCommands = (editor) => {
   const { deleteBackward, insertBreak, isVoid } = editor;
 
-  editor.deleteBackward = (...args) => {
-    const [parentNode, parentPath] = Editor.parent(
+  editor.deleteBackward = (args) => {
+    const [parentNode] = Editor.parent(
       editor,
       editor.selection.focus.path,
     );
 
-    if (isVoid(parentNode) || !Node.string(parentNode).length) {
-      Transforms.removeNodes(editor, { at: parentPath });
-    } else {
-      deleteBackward(...args);
+    if (!editor.selection || editor.selection.anchor.offset !== 0) return deleteBackward(args);
+    const prevNodePath = Path.previous(Path.parent(editor.selection.anchor.path));
+    const prevNode = Node.get(editor, prevNodePath);
+    if (Editor.isVoid(editor, prevNode) || !Node.string(parentNode).length) {
+      return Transforms.removeNodes(editor);
     }
+
+    deleteBackward(args);
   };
 
   editor.insertBreak = (...args) => {
